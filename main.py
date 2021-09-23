430364f552d600f84a5b37ae97938450ce4d6cf8
DajmondFM <66216008+DajmondFM@users.noreply.github.com>
2021-09-23 21:51:12 +0200

    Create version 0.01

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import discord
+from discord import client
+from discord.ext import commands, tasks
+from discord_slash import SlashCommand
+import random
 import os
-print("█░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   █▀▄ ▄▀█ ░░█ █▀▄▀█ █▀█ █▄░█ █▀▄\n▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   █▄▀ █▀█ █▄█ █░▀░█ █▄█ █░▀█ █▄▀")
 
-# Kocham Koty
-# <3
-# <3
+client = commands.Bot(command_prefix="$")
+v = "v0.01"
+client.remove_command("help")
+slash = SlashCommand(client, sync_commands=True)
+
+
+@client.event 
+async def on_ready():
+  print("Zalogowaliśmy się do {0.user}".format(client))
+  print("█░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   █▀▄ ▄▀█ ░░█ █▀▄▀█ █▀█ █▄░█ █▀▄\n▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   █▄▀ █▀█ █▄█ █░▀░█ █▄█ █░▀█ █▄▀")
+
+
+client.run("ODkwNjU3MTQ5NDQ3OTY2NzUx.YUy_HA.5pqjnYjhQ6ltrecac-2OL84LZHg")
