f84f66ee545a43919c55d450fb2c0a678d74b377
DajmondFM <66216008+DajmondFM@users.noreply.github.com>
2021-09-23 21:55:14 +0200

    Next Patch

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ async def on_ready():
   print("█░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   █▀▄ ▄▀█ ░░█ █▀▄▀█ █▀█ █▄░█ █▀▄\n▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   █▄▀ █▀█ █▄█ █░▀░█ █▄█ █░▀█ █▄▀")
 
 
-client.run("ODkwNjU3MTQ5NDQ3OTY2NzUx.YUy_HA.OmRai4tp5Ce4vxpPCh6hFYK27eE")
+client.run("ODkwNjU3MTQ5NDQ3OTY2NzUx.YUy_HA.kSkn1g3Ttw9IhtIHsArs_MKM6J0")
