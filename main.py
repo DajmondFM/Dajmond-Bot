0371cbcf924ad46f620d31404ce137610d7be783
DajmondFM <66216008+DajmondFM@users.noreply.github.com>
2021-09-24 14:42:52 +0000

    Edit gitignore

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-import discord
-from discord import client
-from discord.ext import commands, tasks
-from discord_slash import SlashCommand
-import random
+from nextcord.ext import commands
 import os
 
-client = commands.Bot(command_prefix="$")
-v = "v0.01"
-client.remove_command("help")
-slash = SlashCommand(client, sync_commands=True)
 
 
-@client.event 
-async def on_ready():
-  print("Zalogowaliśmy się do {0.user}".format(client))
-  print("█░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   █▀▄ ▄▀█ ░░█ █▀▄▀█ █▀█ █▄░█ █▀▄\n▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   █▄▀ █▀█ █▄█ █░▀░█ █▄█ █░▀█ █▄▀")
 
+bot = commands.Bot(command_prefix='$')
 
-client.run("ODkwNjU3MTQ5NDQ3OTY2NzUx.YUy_HA.kSkn1g3Ttw9IhtIHsArs_MKM6J0")
+@bot.command()
+async def ping(ctx):
+    await ctx.reply('Pong!')
+
+my_secret = os.environ['Token']
+bot.run('my_secret')
