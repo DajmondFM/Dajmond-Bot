299831a64d956b188d9f0af3f0a6105a4349be62
DajmondFM <66216008+DajmondFM@users.noreply.github.com>
2021-09-25 20:54:23 +0000

    Update: Slash commands not working!

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from nextcord.ext import commands
+import discord
+from discord.ext import commands, tasks
+from discord_slash import SlashCommand
+import random
 import os
+from dotenv import load_dotenv
+
+client = commands.Bot(command_prefix="$")
+v = "v0.01"
+uk = "Użyto komendy "
+client.remove_command("help")
+slash = SlashCommand(client, sync_commands=True)
 
 
+@client.event 
+async def on_ready():
+  print("Zalogowaliśmy się do {0.user}".format(client))
+  print("█░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   █▀▄ ▄▀█ ░░█ █▀▄▀█ █▀█ █▄░█ █▀▄\n▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   █▄▀ █▀█ █▄█ █░▀░█ █▄█ █░▀█ █▄▀")
 
+@slash.slash(description="Pokazuje opóźnienie jakie ma bot")
+async def js(ctx):
+  print(uk + "/Ping")
+  await ctx.send(f"Pong {round(client.latency * 1000)}ms")
 
-bot = commands.Bot(command_prefix='$')
-
-@bot.command()
+@client.command(name="ping")
 async def ping(ctx):
-    await ctx.reply('Pong!')
+  print(uk + "Ping")  
+  await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
 
-my_secret = os.environ['Token']
-bot.run('my_secret')
+load_dotenv()
+TOKEN = os.getenv("Token")
+client.run(TOKEN)
