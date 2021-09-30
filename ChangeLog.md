88c86443ab1bf7a4d107b4ee8835e7c435ba8e26
DajmondFM <66216008+DajmondFM@users.noreply.github.com>
2021-09-30 08:49:52 +0200

    Update to 0.05

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,10 +1,14 @@
 # ChangeLog ENG
 0.01 == First update <br>
-0.02 == Add / command, but it dosen't work <br>
+0.02 == Add slash command, but it isn't working <br>
 0.03 == Add activity status <br>
+0.04 == Fix slash commands <br>
+0.05 == Add button to commands <br>
 
 <br><br>
 # ChangeLog PL
 0.01 == Pierwsza aktualizacja <br>
 0.02 == Dodanie komendy / ale ona nie działa <br> 
 0.03 == Dodanie statusu aktywności <br>
+0.04 == Naprawienie komend slash <br>
+0.05 == Dodanie przycisków do komend <br>
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import discord
+from discord import channel
 from discord.ext import commands, tasks
-from discord_slash import SlashCommand
+from discord_slash import *
+from discord_buttons_plugin import  *
 import random
 import os
 from dotenv import load_dotenv
 
 client = commands.Bot(command_prefix="$")
-v = "v0.03"
+v = "v0.05"
 uk = "Użyto komendy "
-client.remove_command("help")
+# client.remove_command("help")
+buttons = ButtonsClient(client)
 slash = SlashCommand(client, sync_commands=True)
 
 
@@ -18,17 +21,68 @@ async def on_ready():
   print("Zalogowaliśmy się do {0.user}".format(client))
   print("█░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   █▀▄ ▄▀█ ░░█ █▀▄▀█ █▀█ █▄░█ █▀▄\n▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   █▄▀ █▀█ █▄█ █░▀░█ █▄█ █░▀█ █▄▀")
 
-@slash.slash(description="Pokazuje opóźnienie jakie ma bot")
+@slash.slash(name="ping", description="Pokazuje opóźnienie jakie ma bot")
 async def js(ctx):
   print(uk + "/Ping")
   await ctx.send(f"Pong {round(client.latency * 1000)}ms")
+
+@slash.slash(description="TEST")
+async def test(ctx):
+  await ctx.send("test")
 
 @client.command(name="ping")
 async def ping(ctx):
   print(uk + "Ping")  
   await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
 
+@client.command()
+async def test(ctx):
+  await ctx.reply("test")
+
+
+
+
+# TEST
+@client.command()
+async def status(ctx):
+  await buttons.send(
+    content="Wybierz status!",
+    channel=ctx.channel.id,
+    components=[
+      ActionRow([
+        Button(
+          label="Gra",
+          style=ButtonType().Primary,
+          custom_id="game"),
+        Button(
+          label="Stream",
+          style=ButtonType().Primary,
+          custom_id="live"),
+        Button(
+          label="Słuchanie muzyki",
+          style=ButtonType().Primary,
+          custom_id="music"),
+        Button(
+          label="Oglądanie YT",
+          style=ButtonType().Primary,
+          custom_id="film")
+      ])
+    ]
+  )
+
+@buttons.click
+async def game(ctx):
+  await client.change_presence(activity=discord.Game(name="Minecraft <3")) 
+@buttons.click
+async def live(ctx):
+  await client.change_presence(activity=discord.Streaming(name="Life", url="https://www.twitch.tv/dajmondnull"))
+@buttons.click
+async def music(ctx):
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Polish Rap"))
+@buttons.click
+async def film(ctx):
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.custom, name="YouTube"))
+
 load_dotenv()
-TOKEN = os.getenv
-("Token")
+TOKEN = os.getenv("Token")
 client.run(TOKEN)
